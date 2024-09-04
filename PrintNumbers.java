0619cb1c5771449e78bd5fda8fd0f1e45ea5058f
Md Abdur Rahim <98558957+marahim34@users.noreply.github.com>
2024-09-05 00:56:27 +0300

    first codes with JAVA

---
+++ PrintNumbers.java
@@ -0,0 +1,9 @@
+public class PrintNumbers {
+    public static void main(String[] args) {
+        System.out.println(100);
+        System.out.println(200);
+        System.out.println(300);
+        System.out.println(3 + 3);
+        System.out.println(2 * 5);
+    }
+}
---
+++ Product.java
@@ -0,0 +1,8 @@
+public class Product {
+    public static void main(String[] args) {
+
+        System.out.println("id: 101\ntitle: iphone15\n" + //
+                "price 1895 euros,\ndescription: perfect produt");
+
+    }
+}
